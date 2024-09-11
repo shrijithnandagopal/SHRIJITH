75a4a17a73f58e7a24d3f8dbba7a2227207e8688
shrijithnandagopal <s224742799@deakin.edu.au>
2024-09-11 18:20:46 +1000

    Delete login-registration directory

--- login-registration/src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';  // Optional for global styles
-
-ReactDOM.render(<App />, document.getElementById('root'));
